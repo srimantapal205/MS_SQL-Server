aac0d27b775638f6eea4c53673863fcda2c01e40
Srimanta Pal <srimantapal205@hotmail.com>
2025-01-18 11:51:25 +0530

    Update Forlder_name and file

---
+++ 02_SQL Server – Clauses/GroupByClause.sql
@@ -0,0 +1,19 @@
+USE WhereClause;
+-- The GROUP BY clause in SQL Server is used to group rows that have the same values in specified columns into summary rows, like totals or counts. It is typically used in conjunction with aggregate functions such as COUNT, SUM, AVG, MAX, or MIN to perform calculations on each group of data.
+
+-- SELECT column1, column2, AGGREGATE_FUNCTION(column3)
+-- FROM table_name
+-- WHERE condition
+-- GROUP BY column1, column2
+-- HAVING condition_on_aggregate_function
+-- ORDER BY column1, column2;
+
+
+-- --------------------------
+-- SELECT expression1, expression2, expression_n, 
+--        aggregate_function (expression)
+-- FROM tables
+-- [WHERE conditions]
+-- GROUP BY expression1, expression2, expression_n;
+
+-- -----------------------------------------------------
