fa60632be24a4e07d9fda3eeb7db1e90ac392501
Srimanta Pal <srimantapal205@hotmail.com>
2025-02-11 21:14:04 +0530

    How Do you remove the duplicates from a table

--- SQL_CODE/Advanced_Sql_Concept_Page_1.sql
+++ SQL_CODE/Advanced_Sql_Concept_Page_1.sql
@@ -9,3 +9,11 @@ SELECT MAX(ID) AS Last_Id FROM Employee;
 SELECT * FROM Employee WHERE ID = (SELECT MAX(ID) AS Last_Id FROM Employee) ;
 
 SELECT TOP 1 ID FROM Employee ORDER BY ID DESC;
+
+
+-- How Do you remove the duplicates from a table
+-- Using DISTINCT
+SELECT * FROM dbo.tblOrder;
+
+SELECT DISTINCT * FROM dbo.tblOrder;
+-- This Will retrive distinct row from the table based on all column.
