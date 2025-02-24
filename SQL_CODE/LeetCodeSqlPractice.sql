d8db8d12934c71599345af6c93f9ca48a1416a78
Srimanta Pal <srimantapal205@hotmail.com>
2025-02-24 21:16:41 +0530

    Create table person and address table

--- SQL_CODE/LeetCodeSqlPractice.sql
+++ SQL_CODE/LeetCodeSqlPractice.sql
@@ -4,3 +4,27 @@ CREATE DATABASE CommonPracticDB;
 USE CommonPracticDB
 GO
 
+CREATE  TABLE Person (
+	PersonId INT IDENTITY(1,1) CONSTRAINT person_pk PRIMARY KEY ,
+	FirstName VARCHAR(30),
+	LastName VARCHAR(30)
+)
+INSERT INTO Person(FirstName, LastName) VALUES  ('John', 'Doe'),
+    ('Jane', 'Smith'),
+    ('Michael', 'Johnson'),
+    ('Emily', 'Davis'),
+    ('David', 'Brown');
+
+
+CREATE TABLE AddressBook(
+	AddreeId INT IDENTITY(1,1) CONSTRAINT addreess_pk PRIMARY KEY,
+	PersonID INT CONSTRAINT personid_fk FOREIGN KEY (PersonId) REFERENCES Person(PersonId),
+	City VARCHAR(30),
+	State VARCHAR(30)
+)
+
+INSERT INTO AddressBook(PersonID, City, State ) VALUES (1, 'New York', 'NY'),
+    (2, 'Los Angeles', 'CA'),
+    (3, 'Chicago', 'IL'),
+    (4, 'Houston', 'TX'),
+    (5, 'Miami', 'FL');
