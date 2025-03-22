a1931b26963e2560474e1942af176e6157951414
Srimanta Pal <srimantapal205@hotmail.com>
2025-03-22 06:45:16 +0530

    add row num rank case window function

--- SQL_CODE/Sql_code_22_03-2025.sql
+++ SQL_CODE/Sql_code_22_03-2025.sql
@@ -5,3 +5,27 @@ USE WhereClause;
 
 SELECT name FROM sys.tables
 
+SELECT * FROM Employee;
+
+-- Using Case
+
+SELECT ID, Name,
+	CASE
+		WHEN Salary > 70000 THEN 'High'
+		WHEN Salary BETWEEN 45000 AND 70000 THEN 'Medium'
+		ELSE 'LOW'
+	END AS SalaryLevel
+FROM Employee
+
+
+SELECT *, COALESCE(NULL, NULL, 'Default Value') AS Result FROM Employee;
+
+SELECT ID, Name, NVL2(Salary, 0) AS AdjustSalary FROM Employee
+
+SELECT ID, Name, Salary, RANK() OVER(ORDER BY Salary DESC) AS Rank FROM Employee;  -- Using RANK() function it will skip the rank number if get the ties value
+
+SELECT ID, Name, Salary, DENSE_RANK() OVER(ORDER BY Salary DESC) AS DenseRank FROM Employee; -- Using DENSE_RANK() function provide rank sequencialy without any skip or gap. 
+
+SELECT Salary, DENSE_RANK() OVER(ORDER BY Salary DESC) AS DenseRank FROM Employee GROUP BY Salary;  
+
+SELECT ID, Name, Salary, DENSE_RANK() OVER(ORDER BY Salary DESC) As Emp_Rank, ROW_NUMBER() OVER(ORDER BY ID) AS Emp_Row_Number FROM Employee; -- USING Row number and rank to compare 
