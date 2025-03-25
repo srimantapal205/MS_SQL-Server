e1b91fafecd73b3c72941c486cb839e79eba9e8d
Srimanta Pal <srimantapal205@hotmail.com>
2025-03-25 06:29:28 +0530

    Practic 10 query

--- SQL_CODE/MarketPlace.sql
+++ SQL_CODE/MarketPlace.sql
@@ -39,7 +39,43 @@ INSERT INTO Products (product_id, product_name, category, unit_price) VALUES
 (104, 'Keyboard', 'Electronics', 20.00),
 (105, 'Mouse', 'Electronics', 15.00);
 
-
+-- 1. Retrieve all columns from the tables
 SELECT * FROM Sales;
 SELECT * FROM Products;
 
+-- 2. Retrieve the product_name and unit_price from the Products table.
+SELECT product_name, unit_price FROM Products;
+-- 3. Retrieve the sale_id and sale_date from the Sales table.
+SELECT sale_id, sale_date FROM Sales ;
+
+-- 4. Filter the Sales table to show only sales with a total_price greater than $100
+SELECT * FROM Sales WHERE total_price > 100;
+
+-- 5. Filter the Products table to show only products in the 'Electronics' category.
+SELECT * FROM Products WHERE category = 'Electronics'
+
+-- 6. Retrieve the sale_id and total_price from the Sales table for sales made on January 3, 2024.
+SELECT sale_id, total_price FROM Sales WHERE sale_date = '2024-01-03';
+
+-- 7. Retrieve the product_id and product_name from the Products table for products with a unit_price greater than $100.
+SELECT product_id, product_name FROM Products WHERE unit_price > 100;
+
+-- 8. Calculate the total revenue generated from all sales in the Sales table.
+
+SELECT SUM(total_price) AS total_revenue FROM Sales;
+
+-- 9. Calculate the average unit_price of products in the Products table.
+SELECT * FROM Products;
+SELECT AVG(unit_price) AS average_unit_price FROM Products; -- 173.000000
+
+--ROUND function (for fixed decimal places):
+SELECT ROUND(AVG(unit_price), 2) AS average_unit_price FROM Products; -- 173.000000
+--CAST function (for integer output):
+SELECT CAST(AVG(unit_price) AS INT) AS average_unit_price FROM Products; -- 173 
+
+
+-- 10. Calculate the total quantity_sold from the Sales table.
+
+SELECT * FROM Sales;
+
+SELECT SUM(quantity_sold) AS quantity_sold FROM Sales;
