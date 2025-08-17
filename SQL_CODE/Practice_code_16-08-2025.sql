cd5516b692a0d9f2723ff97357a02ca3242efc43
Srimanta Pal <srimantapal205@hotmail.com>
2025-08-17 21:55:42 +0530

    Update date query

--- SQL_CODE/Practice_code_16-08-2025.sql
+++ SQL_CODE/Practice_code_16-08-2025.sql
@@ -59,36 +59,39 @@ FROM cteReports r
 LEFT JOIN Employees m ON r.ManagerID = m.EmployeeID
 ORDER BY r.EmpLevel, r.ManagerID;
 
-
+--  CTE to generate a date dimension table:
 /*
-WITH cteReports (EmployeeID, FirstName, LastName, MgrID, EmpLevel) AS (
-    SELECT 
-        EmployeeID, 
-        FirstName, 
-        LastName, 
-        ManagerID, 
-        1 AS EmpLevel
-    FROM Employees 
-    WHERE ManagerID IS NULL
-    
-    UNION ALL 
-    
-    SELECT 
-        e.EmployeeID, 
-        e.FirstName, 
-        e.LastName, 
-        e.ManagerID, 
-        r.EmpLevel + 1
-    FROM Employees e
-    INNER JOIN cteReports r ON e.ManagerID = r.EmployeeID
+WITH dates (Date) AS (
+	SELECT 
+		CAST('2024-01-01' as date) AS Date, 
+		DATEPART('year', cast('2024-01-01' as date)) as year, 
+		DATEPART('month', cast('2024-01-01' as date)) as month
+	UNION ALL
+	SELECT 
+		DATEADD(Date, INTERVAL 1 day), 
+		date('year', date_add(Date, INTERVAL 1 day)) AS year,
+		date_part('month', date_add(Date, INTERVAL 1 day)) AS month
+	FROM dates
+	WHERE Date < '2025-12-31'
 )
-SELECT 
-    r.FirstName + ' ' + r.LastName AS FullName,
-    r.EmpLevel,
-    m.FirstName + ' ' + m.LastName AS Manager
-FROM cteReports r
-LEFT JOIN Employees m ON r.MgrID = m.EmployeeID
-ORDER BY r.EmpLevel, r.MgrID;
-
+SELECT * FROM dates
 */
+
+
+WITH dates AS (
+    SELECT 
+        CAST('2024-01-01' AS date) AS DateVal,
+        DATEPART(YEAR, CAST('2024-01-01' AS date)) AS YearVal,
+        DATEPART(MONTH, CAST('2024-01-01' AS date)) AS MonthVal
+    UNION ALL
+    SELECT 
+        DATEADD(DAY, 1, DateVal),
+        DATEPART(YEAR, DATEADD(DAY, 1, DateVal)),
+        DATEPART(MONTH, DATEADD(DAY, 1, DateVal))
+    FROM dates
+    WHERE DateVal < '2025-12-31'
+)
+SELECT *
+FROM dates
+OPTION (MAXRECURSION 0);  -- needed to go beyond 100 recursion limit
 
