703d3f8119508f27d9202ac7b69758c8c141b8ab
Srimanta Pal <srimantapal205@hotmail.com>
2025-08-17 19:09:20 +0530

    uadate file

--- SQL_CODE/Practice_code_16-08-2025.sql
+++ SQL_CODE/Practice_code_16-08-2025.sql
@@ -22,4 +22,73 @@ INSERT INTO Employees VALUES (9, 'Avery', 'Sinclair', 6);
 INSERT INTO Employees VALUES (10, 'Oliver', 'Beckett', 6);
 
 
+-- The code below will generate a result containing information about an employee and his manager.
+SELECT * FROM Employees;
+
+WITH cteReports (EmployeeID, FirstName, LastName, ManagerID, EmpLevel) AS 
+	(
+		SELECT 
+			EmployeeID, 
+			FirstName, 
+			LastName, 
+			ManagerID, 1 
+		FROM 
+			Employees 
+		WHERE 
+			ManagerID IS NULL 
+		UNION 
+			ALL 
+		SELECT 
+			e.EmployeeID, 
+			e.FirstName, 
+			e.LastName, 
+			e.ManagerID, 
+			r.EmpLevel + 1 
+		FROM 
+			Employees e 
+		INNER JOIN 
+			cteReports r 
+		ON 
+			e.ManagerID = r.EmployeeID
+)
+SELECT 
+    r.FirstName + ' ' + r.LastName AS FullName,
+    r.EmpLevel,
+    m.FirstName + ' ' + m.LastName AS Manager
+FROM cteReports r
+LEFT JOIN Employees m ON r.ManagerID = m.EmployeeID
+ORDER BY r.EmpLevel, r.ManagerID;
+
+
+/*
+WITH cteReports (EmployeeID, FirstName, LastName, MgrID, EmpLevel) AS (
+    SELECT 
+        EmployeeID, 
+        FirstName, 
+        LastName, 
+        ManagerID, 
+        1 AS EmpLevel
+    FROM Employees 
+    WHERE ManagerID IS NULL
+    
+    UNION ALL 
+    
+    SELECT 
+        e.EmployeeID, 
+        e.FirstName, 
+        e.LastName, 
+        e.ManagerID, 
+        r.EmpLevel + 1
+    FROM Employees e
+    INNER JOIN cteReports r ON e.ManagerID = r.EmployeeID
+)
+SELECT 
+    r.FirstName + ' ' + r.LastName AS FullName,
+    r.EmpLevel,
+    m.FirstName + ' ' + m.LastName AS Manager
+FROM cteReports r
+LEFT JOIN Employees m ON r.MgrID = m.EmployeeID
+ORDER BY r.EmpLevel, r.MgrID;
+
+*/
 
