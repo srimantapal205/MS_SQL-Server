657d5a64ba1c29c78d14ec1ad4b25cbffeb77b55
Srimanta Pal <srimantapal205@hotmail.com>
2025-11-19 05:48:36 +0530

    Quarterly Summary and Half-Yearly Summary

--- SQL_CODE/Transaction/PracticeQuery.sql
+++ SQL_CODE/Transaction/PracticeQuery.sql
@@ -136,3 +136,87 @@ GROUP BY
     DATEPART(DAY, PaymentDate)
 ORDER BY
     YearOfYear, YearOfMonth, MonthOfDay, HourOfDay, MinuteOfHour, SecondOfMinute; 
+
+-- Quarterly Summary
+-- (Q1 = Jan–Mar, Q2 = Apr–Jun, Q3 = Jul–Sep, Q4 = Oct–Dec)
+
+WITH combineTable AS (
+	SELECT 
+        o.OrderID, 
+        o.CustomerID, 
+        p.PaymentID, 
+        o.OrderDate, 
+        o.TotalAmount,        
+        p.PaymentDate, 
+        p.PaymentMethod, 
+        p.Amount AS PaidAmount, 
+        p.Status AS PaymentStatus
+    FROM 
+        Orders AS o 
+    JOIN 
+        Payments AS p 
+    ON 
+        o.OrderID = p.OrderID
+),
+transactions AS(
+    SELECT CustomerID, PaymentDate, PaidAmount FROM combineTable
+)
+--SELECT * FROM transactions
+SELECT 
+      DATEPART(YEAR, PaymentDate)  AS Year_By,
+      CONCAT('Q',DATEPART(QUARTER, PaymentDate)) AS QuarterlyBy_3_Txn,
+      COUNT(*) AS Txn_count,
+      SUM(PaidAmount) AS Total_Amount
+FROM 
+    transactions
+GROUP BY 
+    DATEPART(YEAR, PaymentDate),
+    CONCAT('Q',DATEPART(QUARTER, PaymentDate))
+ORDER BY 
+    Year_By DESC,
+    QuarterlyBy_3_Txn;
+
+
+-- Half-Yearly Summary
+-- Half-year groups: H1 = Jan–Jun, H2 = Jul–Dec
+
+WITH combineTable AS (
+	SELECT 
+        o.OrderID, 
+        o.CustomerID, 
+        p.PaymentID, 
+        o.OrderDate, 
+        o.TotalAmount,        
+        p.PaymentDate, 
+        p.PaymentMethod, 
+        p.Amount AS PaidAmount, 
+        p.Status AS PaymentStatus,
+        CASE
+        WHEN
+            DATEPART(MONTH, p.PaymentDate) BETWEEN 1 AND 6 THEN 'H1'
+        ELSE
+            'H2'
+    END AS Hals_year_Txn
+    FROM 
+        Orders AS o 
+    JOIN 
+        Payments AS p 
+    ON 
+        o.OrderID = p.OrderID
+),
+transactions AS(
+    SELECT CustomerID, PaymentDate, PaidAmount, Hals_year_Txn FROM combineTable
+)
+-- SELECT * FROM transactions;
+SELECT 
+    DATEPART(YEAR, PaymentDate) AS Txn_Year,
+    COUNT(*) AS Count_Txn,
+    SUM(PaidAmount) AS Total_amount, 
+    Hals_year_Txn
+FROM 
+    transactions
+GROUP BY
+    DATEPART(YEAR, PaymentDate),
+    Hals_year_Txn
+ORDER BY
+    Txn_Year, Hals_year_Txn;
